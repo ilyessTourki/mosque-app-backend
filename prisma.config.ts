43dce3b51bf0a791813ea827cb57b11528a3cd2b
Ilyess Tourki <38595113+ilyessTourki@users.noreply.github.com>
2026-05-17 23:07:59 +0200

    fix: pass DATABASE_URL to prisma migrate in config

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'prisma/config'
 export default defineConfig({
   earlyAccess: true,
   schema: 'prisma/schema.prisma',
-  datasource: {
-    url: process.env['DATABASE_URL']!,
+  migrate: {
+    async beforeApply() {
+      return {
+        datasourceUrl: process.env['DATABASE_URL']!,
+      }
+    },
   },
 })
