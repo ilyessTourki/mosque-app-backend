dd0440efd8245f0127e8230917fadeea2604ecd6
Ilyess Tourki <38595113+ilyessTourki@users.noreply.github.com>
2026-05-17 23:14:34 +0200

    fix: remove earlyAccess and add datasource.url for prisma v7

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'prisma/config'
-
 export default defineConfig({
-  earlyAccess: true,
   schema: 'prisma/schema.prisma',
-  migrate: {
-    async beforeApply() {
-      return {
-        datasourceUrl: process.env['DATABASE_URL']!,
-      }
-    },
+  datasource: {
+    url: process.env['DATABASE_URL']!,
   },
 })
