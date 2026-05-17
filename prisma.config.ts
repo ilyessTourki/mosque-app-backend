f3c8f54d3970672f867bfd5c73068dcc9ad7c5b6
Ilyess Tourki <38595113+ilyessTourki@users.noreply.github.com>
2026-05-17 22:59:25 +0200

    fix: add datasource url to prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,9 @@
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
+  earlyAccess: true,
+  schema: 'prisma/schema.prisma',
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: process.env['DATABASE_URL']!,
   },
-});
+})
